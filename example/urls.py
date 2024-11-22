30624d4f39d17fbe4d52e39da54ca193dc08e8a1
Jonathan <Jonathan106e@gmail.com>
2024-11-22 04:31:59 -0600

    mensaje hola para test api

--- example/urls.py
+++ example/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import HolaAPIView
+from .views import HolaAPIView,  MiVista
 
 urlpatterns = [
     path('hola/', HolaAPIView.as_view(), name='hola'),
+    path('hola/', MiVista.as_view(), name='hola'),
 ]
--- example/views.py
+++ example/views.py
@@ -4,3 +4,12 @@ from rest_framework.response import Response
 class HolaAPIView(APIView):
     def get(self, request):
         return Response({"mensaje": "Hola"})
+
+from rest_framework.permissions import AllowAny
+from rest_framework.views import APIView
+
+class MiVista(APIView):
+    permission_classes = [AllowAny]
+
+    def get(self, request):
+        return Response({"mensaje": "Acceso permitido para todos"})
